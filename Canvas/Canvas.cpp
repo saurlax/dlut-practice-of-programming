5a7b455c724e5cf6de18a217a44a49d95f1f5fef
Saurlax <saurlax@qq.com>
2024-07-02 19:30:48 +0800

    feat: add canvas

---
+++ Canvas/Canvas.cpp
@@ -0,0 +1,98 @@
+#include <graphics.h>
+#include <string>
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.1415926
+
+using namespace std;
+
+int main()
+{
+	bool running = true;
+	bool updated = true;
+
+	bool square = false;
+	bool circle = false;
+	int width = 10, x, y;
+
+	ExMessage msg;
+
+	initgraph(800, 600);
+
+	cleardevice();
+
+	while (running) {
+		while (peekmessage(&msg)) {
+			if (msg.message == WM_CLOSE) running = false;
+			else if (msg.message == WM_KEYDOWN) {
+				switch (msg.vkcode) {
+				case VK_ESCAPE:
+					running = false;
+					break;
+				case VK_CONTROL:
+					width = 20;
+					break;
+				case 'c':
+				case 'C':
+					cleardevice();
+					break;
+				case 'r':
+				case 'R':
+					setfillcolor(RED);
+					break;
+				case 'g':
+				case 'G':
+					setfillcolor(GREEN);
+					break;
+				case 'b':
+				case 'B':
+					setfillcolor(BLUE);
+					break;
+				case 'w':
+				case 'W':
+					setfillcolor(WHITE);
+					break;
+				}
+			}
+			else if (msg.message == WM_KEYUP && msg.vkcode == VK_CONTROL) {
+				width = 10;
+			}
+			else if (msg.message == WM_LBUTTONDOWN) {
+				square = true;
+				updated = true;
+			}
+			else if (msg.message == WM_RBUTTONDOWN) {
+				circle = true;
+				updated = true;
+			}
+			else if (msg.message == WM_LBUTTONUP) {
+				square = false;
+				updated = true;
+			}
+			else if (msg.message == WM_RBUTTONUP) {
+				circle = false;
+				updated = true;
+			}
+			else if (msg.message == WM_MOUSEMOVE) {
+				x = msg.x, y = msg.y;
+				updated = true;
+			}
+		}
+
+		if (updated) {
+			updated = false;
+			BeginBatchDraw();
+			if (square) {
+				solidrectangle(x - width / 2, y - width / 2, x + width / 2, y + width / 2);
+			}
+			if (circle) {
+				solidcircle(x, y, width);
+			}
+			EndBatchDraw();
+		}
+	}
+
+	closegraph();
+	return 0;
+}
